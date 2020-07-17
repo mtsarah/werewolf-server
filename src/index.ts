281105a708fe0258df3b6bfd5df245afcc64d854
mtsarah <bers@xxxlgroup.com>
2020-07-17 20:43:26 +0200

    feat: change listening port to 8666

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ app.get("/", function(req, res) {
   res.send("Hello World!");
 });
 
-app.listen(3000, function() {
-  console.log("App is listening on port 3000!");
+app.listen(8666, function() {
+  console.log("App is listening on port 8666!");
 });
